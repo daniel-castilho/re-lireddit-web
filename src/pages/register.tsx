4349b9d469119614188d260dfc9546936ac6ec18
Daniel Castilho <dan.castilho@gmail.com>
2021-04-04 22:28:04 -0400

    Connect Frontend with GraphQL

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -1,23 +1,39 @@
-import { Button } from "@chakra-ui/button";
-import { FormControl, FormErrorMessage, FormLabel } from "@chakra-ui/form-control";
-import { Input } from "@chakra-ui/input";
-import { Box } from "@chakra-ui/layout";
-import { Field, Form, Formik } from "formik";
 import React from "react";
+import { Button } from "@chakra-ui/button";
+import { Box } from "@chakra-ui/layout";
+import { Form, Formik } from "formik";
+import { useMutation } from "urql";
 import { InputField } from "../components/InputField";
 import { Wrapper } from "../components/Wrapper";
 
 interface registerProps {}
 
+const REGISTER_MUTATION = `
+mutation Register($username: String!, $password: String!) {
+  register(options: { username: $username, password: $password }) {
+    user {
+      id
+      createdAt
+      updatedAt
+      username
+    }
+    errors {
+      field
+      message
+    }
+  }
+}
+`
+
 export const Register: React.FC<registerProps> = ({}) => {
+  const [,register] = useMutation(REGISTER_MUTATION)
 	return (
     <Wrapper variant="small">
       <Formik
         initialValues={{ username: "", password: "" }}
-        onSubmit={(values, actions) => {
-          setTimeout(() => {
-            console.log(values)
-          })
+        onSubmit={ async(values) => {
+          console.log(values)
+          const response = await register(values)
         }}
       >
         {({ isSubmitting }) => (
