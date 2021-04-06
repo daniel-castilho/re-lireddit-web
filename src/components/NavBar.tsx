e4b526049c59ab3fc47675da04e430c86df041ed
Daniel Castilho <dan.castilho@gmail.com>
2021-04-06 16:44:22 -0400

    Create Query Me to return username into NavBar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,21 +1,43 @@
 import { Box, Flex, Link } from "@chakra-ui/layout";
 import React from "react";
 import NextLink from "next/link";
+import { useMeQuery } from "../generated/graphql"
 
 interface NavBarProps {}
 
 export const NavBar: React.FC<NavBarProps> = ({}) => {
+	const [{ data, fetching }] = useMeQuery()
+	let body = null
+
+	// Data is loading
+	if (fetching) {
+
+	}
+	// User is not logged in
+	else if (!data?.me) {
+		body = (
+			<>
+			<NextLink href="/login">
+			<Link mr={2} ml={"auto"}>
+			login
+			</Link>
+			</NextLink>
+			<NextLink href="/register">
+			<Link>register</Link>
+			</NextLink>
+			</>
+		)
+	}
+	// User is logged in
+	else {
+		body = (
+			<Box>{ data.me.username }</Box>
+		)
+	}
 	return (
 		<Flex bg="tomato" p={4}>
 			<Box ml={"auto"}>
-				<NextLink href="/login">
-					<Link mr={2} ml={"auto"}>
-						login
-					</Link>
-				</NextLink>
-				<NextLink href="/register">
-					<Link>register</Link>
-				</NextLink>
+				{ body }
 			</Box>
 		</Flex>
 	);
